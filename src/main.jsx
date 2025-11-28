00a1fd5aeceb7b27a3d1ac935d1c6a69c6a532a7
busybert <bertyz@gmail.com>
2025-11-28 16:00:12 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import App from '@/App';
-import '@/index.css';
+
+// FIXED – removed alias imports
+import App from './App.jsx';
+import './index.css';
+
+// Leaflet styles
 import 'leaflet/dist/leaflet.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <>
+  <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </>
+  </React.StrictMode>
 );
